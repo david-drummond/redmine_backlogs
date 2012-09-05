9c6bcd19e9db9c822bd2eb767f9804c546d642b5
Daniel Bush <dlb.id.au@gmail.com>
2012-09-05 17:40:25 +1000

    Form now updates per-project sprint task status settings.

--- app/controllers/rb_issue_statuses_controller.rb
+++ app/controllers/rb_issue_statuses_controller.rb
@@ -3,16 +3,61 @@
 class RbIssueStatusesController < RbApplicationController
   unloadable
 
+  # Form for editing per-project sprint task statuses.
+
   def edit
+    setup
+  end
+
+  # Update per-project sprint task statuses.
+  #
+  # If params[:issue_statuses] is empty or not there, then
+  # all per-project task statuses will be removed.
+
+  def update
+    issue_status_ids = params[:issue_statuses]
+    project_name = params[:project_id]
+    unless project_name then
+      render_error "No project id given."
+    end
     @project = Project.find_by_name(params[:project_id])
     unless @project then
-      # TODO: set error.
+      render_error "No project given."
+    end
+    if issue_status_ids.nil? then
+      ids = []
+    else
+      ids = issue_status_ids.keys.map{|k|k.to_i}
+    end
+    RbProjectTaskStatus.update!(@project.id,ids)
+
+    setup @project
+    render :action => 'edit'
+  end
+
+  private
+
+  # For use with edit and update.
+
+  def setup project=nil
+    if project.nil? then
+      @project = Project.find_by_name(params[:project_id])
+    else
+      @project = project
+    end
+    unless @project then
+      render_error "No project given."
     end
     @title = "Issue statuses for #{@project.name}"
     @issue_statuses = IssueStatus.find(:all)
-  end
-
-  def update
+    # Create a lookup of issue statuses for a given project.
+    @project_statuses = RbProjectTaskStatus.find(
+      :all,
+      :conditions => {:project_id => @project.id})
+    @project_statuses = @project_statuses.inject({}){|s,v|
+      s[v[:issue_status_id]] = v
+      s
+    }
   end
 
 end
--- app/models/rb_project_task_status.rb
+++ app/models/rb_project_task_status.rb
@@ -1,3 +1,27 @@
+# Model to represent a project's preferred sprint task statuses
+# for use on the taskboard.
+#
+# By default, projects start with none and will use the default
+# task statuses that are specified in the main Backlogs pluging
+# configuration page.
+#
+# Note: RbSprintTaskTracker will need to be told when a project
+# changes its preferred task statuses here so that it can update
+# itself.
+
 class RbProjectTaskStatus < ActiveRecord::Base
   unloadable
+  belongs_to :project
+  belongs_to :issue_status
+
+  # Update (sprint) task statuses for a given project.
+
+  def self.update! project_id, issue_status_ids=[]
+    self.delete_all({:project_id => project_id})
+    unless issue_status_ids.empty? then
+      issue_status_ids.each {|i|
+        self.create!(:project_id => project_id,:issue_status_id => i)
+      }
+    end
+  end
 end
